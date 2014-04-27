e7834e67b7800e3e662cf6742b543fbbd1249e79
Martin Sookael <m@451.ee>
2014-04-27 15:36:21 +0300

    hide the logout notificaitons

--- t.js
+++ t.js
@@ -193,13 +193,13 @@ io.sockets.on('connection', function (socket) {
     });
 
     socket.on('logout', function () {
-      removeUser(socket.username);
+      /*removeUser(socket.username);
 
       // echo globally that this client has left
       // remove disconnected because remove disconnected
       io.sockets.emit('news', { title: '<strong>'+socket.username + '</strong> has disconnected', author: 'Server', time: 'bye'});
       socket.leave(socket.room); // turned that off, since other way started havocing
-      socket.emit('logout');
+      socket.emit('logout');*/
     });
 
     socket.on('help', function () {
