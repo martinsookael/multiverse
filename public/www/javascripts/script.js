f913e739df4678406c822041b1a7412be10ac552
Martin Sookael <m@451.ee>
2014-07-10 16:50:23 +0300

    trying to get posting last 7 to work

--- public/www/javascripts/script.js
+++ public/www/javascripts/script.js
@@ -502,8 +502,8 @@ multiverse.controller('one', function($scope, $route, $routeParams, $location) {
   $scope.roomer = function(htmlForm) {
     var room = $scope.roomer.number;
     room = String(room);
-    //var goToRoom = "http://www.multiverse.im/#/r/"+room;
-    var goToRoom = "http://localhost:3001/#/r/"+room;
+    var goToRoom = "http://www.multiverse.im/#/r/"+room;
+    //var goToRoom = "http://localhost:3001/#/r/"+room;
     //$scope.$apply( $location.path( goToRoom ) );
     //$location.path( goToRoom );
     window.location.href = goToRoom;
