fbcf347efd4d1e7c419658a7fac1bd4374d019c6
Martin Sookael <m@451.ee>
2014-07-11 11:05:43 +0300

    navigating with conf.js file

--- conf.js
+++ conf.js
@@ -1,11 +1,9 @@
 module.exports = {
     general: {
-          port: 3001
-//      ,  host: '127.0.0.1' // notice the missing "http://"
-//      , url: 'http://127.0.0.1:3001'
+      port: 3001
     }
   , db: {
-        usesDb: true // true - uses db, false - uses not. 
+        usesDb: true // true - uses db, false - uses not.
       , host: process.env.HOST // notice the missing "http://"
       , port: process.env.DBPORT
       , dbName: process.env.DB
